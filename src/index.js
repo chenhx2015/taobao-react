e6cdfd74912b46558a6e545570098c76dcd8d084
chenhuaxiang@qianshengqian.com <chenhuaxiang@qianshengqian.com>
2018-09-25 15:23:22 +0800

    添加redux-persist中间件对store进行持久化

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom'
 import 'normalize.css'
 import './index.css'
@@ -8,17 +8,47 @@ import registerServiceWorker from './registerServiceWorker'
 import { applyMiddleware, createStore } from 'redux'
 
 import logger from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import taobaoApp from './store/reducer'
 
-let store = createStore(
-    taobaoApp, 
-    applyMiddleware(logger)
+const persistConfig = {
+    key: 'root',
+    storage
+}
+const persistedReducer = persistReducer(
+    persistConfig,
+    taobaoApp
 )
+
+// export default () => {
+//     let store = createStore(persistedReducer, applyMiddleware(logger))
+//     let persistor = persistStore(store)
+//     return { store, persistor }
+// }
+
+let store = createStore(persistedReducer, applyMiddleware(logger))
+let persistor = persistStore(store)
+
+// let store = createStore(
+//     taobaoApp,
+//     applyMiddleware(logger)
+// )
+
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <App />
+//     </Provider>,
+//     document.getElementById('root')
+// )
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 )
